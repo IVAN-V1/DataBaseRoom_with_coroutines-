a7d14e40437c1dedf2700ea49939d8b0bb46aec8
ivan V <cuentabim1@gmail.com>
2025-04-04 17:25:58 -0600

    Create package data

---
+++ app/src/main/java/com/devtools/database_room/data/AppDatabase.kt
@@ -0,0 +1,47 @@
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [ModelUser::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun userDao(): UserDao
+
+
+    /***
+     * companion object: Implementa un patrón Singleton para asegurar que solo haya una instancia de la base de datos en toda la aplicación. Esto es una práctica común para las bases de datos Room
+
+     */
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        /***
+         *
+         * getDatabase(context: android.content.Context):
+         * Un método estático para obtener la instancia de la base de datos. Utiliza Room.databaseBuilder para crear la base de datos si aún no existe. El nombre del archivo de la base de datos será "app_database".
+         *
+         *
+         */
+
+
+        fun getDatabase(context: android.content.Context): AppDatabase {
+
+            return INSTANCE ?: synchronized(this) {
+
+                val instance = androidx.room.Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+
+
+    }
+
+}
---
+++ app/src/main/java/com/devtools/database_room/data/UserDao.kt
@@ -0,0 +1,24 @@
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface UserDao {
+
+
+    @Query("SELECT * FROM users")
+    fun getAll(): List<ModelUser>
+
+    @Insert
+    fun insertAll(vararg users: ModelUser)
+
+    @Query("SELECT * FROM users WHERE id IN (:userIds)")
+    fun loadAllByIds(userIds: IntArray): List<ModelUser>
+
+    @Query("SELECT * FROM users WHERE first_name LIKE :first AND last_name LIKE :last LIMIT 1")
+    fun findByName(first: String, last: String): ModelUser
+
+    @Query("DELETE FROM users")
+    fun deleteAll()
+
+}
