acf280c78588e2a0b001d23d5a99d4d2014f5afb
ivan V <cuentabim1@gmail.com>
2025-04-04 17:17:58 -0600

    Import dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    //ksp [Room]
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -58,6 +60,12 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
 
 
+
+    implementation(libs.kotlinx.coroutines.android)
+    // Room
+    implementation(libs.androidx.room.runtime)
+    implementation (libs.androidx.room.ktx)
+    // KSP
 
 
 
